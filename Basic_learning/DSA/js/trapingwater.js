25187118a62d52e2f69c8c9181fa18c343429b7d
Rishabh <rishabhagarwal2005@gmail.com>
2021-08-10 20:34:06 +0530

    Working solution

--- Basic_learning/DSA/js/trapingwater.js
+++ Basic_learning/DSA/js/trapingwater.js
@@ -1,22 +1,33 @@
 const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 
-const maximumLeft = function(height) {
-    return(Math.max(...height));
-}
-
-const maximumRight = function(height) {
-    return(Math.max(...height));
-}
+// const maximum = function(height) {
+//     return(Math.max(...height));
+// }
 
 const maximumUnits = function (height) {
     let result = 0;
     for (let i = 1; i < height.length - 1; i++) {
-        const [leftArray, rightArray] = [height.slice(0, i), height.slice(i + 1, height.length)];
-        const maxLeft = maximumLeft(leftArray);
-        const maxRight = maximumRight(rightArray);
-        const minimum = Math.min(maxLeft, maxRight) - height[i];
-        if (minimum > 0) {
-            result += minimum;
+        // const maxLeft = maximum(height.slice(0, i));
+        // const maxRight = maximum(height.slice(i + 1, height.length));
+        // const minimum = Math.min(maxLeft, maxRight) - height[i];
+        // if (minimum > 0) {
+        //     result += minimum;
+        // }
+        let leftPointer = i, rightPointer = i, maxLeft = 0, maxRight = 0;
+        while (leftPointer >= 0) {
+            maxLeft = Math.max(maxLeft, height[leftPointer]);
+            leftPointer--;
+        }
+
+        while (rightPointer < height.length) {
+            maxRight = Math.max(maxRight, height[rightPointer]);
+            rightPointer++;
+        }
+
+        const min = Math.min(maxLeft, maxRight) - height[i]; 
+
+        if (min > 0) {
+            result += min;
         }
     }
     return result;
