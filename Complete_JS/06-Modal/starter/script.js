54cd9fd88deeef4b7b1587f9a9aef58b1277a88d
Rishabh <rishabhagarwal2005@gmail.com>
2021-08-11 21:36:14 +0530

    11/08/2021

--- Complete_JS/06-Modal/starter/script.js
+++ Complete_JS/06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const modals = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const hideModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < modals.length; i++) {
+    modals[i].addEventListener('click', openModal);
+}
+
+closeModal.addEventListener('click', hideModal);
+
+overlay.addEventListener('click', hideModal);
