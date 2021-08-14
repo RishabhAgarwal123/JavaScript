61a65fc620a7dc1a6615ac29b42da3662c3b4dd7
Rishabh <rishabhagarwal2005@gmail.com>
2021-08-14 18:29:20 +0530

    14/08/2021

--- PersonalWebsite/assets/js/main.js
+++ PersonalWebsite/assets/js/main.js
@@ -1,3 +1,4 @@
+'use strict'
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu');
 const navClose = document.getElementById('nav-close');
@@ -22,7 +23,7 @@ if (navClose) {
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link');
 
-const actionOnLink = function() {
+const actionOnLink = function () {
     const navMenu = document.getElementById('nav-menu');
     // Remove show menu on click on nav__link
     navMenu.classList.remove('show-menu');
@@ -31,10 +32,45 @@ const actionOnLink = function() {
 navLink.forEach(event => event.addEventListener('click', actionOnLink));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skilledContent = document.getElementsByClassName('skills__content');
+const skillsHeaders = document.querySelectorAll('.skills__header');
 
+const toggleSkills = function () {
+    let itemClass = this.parentNode.className;
+
+    for (let i = 0; i < skilledContent.length; i++) {
+        skilledContent[i].className = 'skills__content skills__close';
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeaders.forEach((event) => {
+    event.addEventListener('click', toggleSkills)
+});
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        });
+
+        target.classList.add('qualification__active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active');
+        });
+        tab.classList.add('qualification__active');
+    });
+});
 
 /*==================== SERVICES MODAL ====================*/
 
@@ -48,10 +84,10 @@ navLink.forEach(event => event.addEventListener('click', actionOnLink));
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
