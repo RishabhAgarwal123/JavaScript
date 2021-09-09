2427883f2c6099aa02d7c2820c1a33ecc0b29e28
Rishabh <rishabhagarwal2005@gmail.com>
2021-09-09 21:15:41 +0530

    Advanced DOM

--- Complete_JS/13-Advanced-DOM-Bankist/starter/script.js
+++ Complete_JS/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const btnToScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const openModal = function (event) {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +22,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal))
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +35,60 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+btnToScroll.addEventListener('click', function (event) {
+  // Old Ways
+  // const coords = section1.getBoundingClientRect();
+  // window.scrollTo(coords.left + window.pageXOffset, coords.top + window.pageYOffset);
+  // window.scrollTo({
+  //   left: coords.left + window.pageXOffset,
+  //   top: coords.top + window.pageYOffset,
+  //   behavior: 'smooth'
+  // })
+  // New Ways
+  section1.scrollIntoView({ behavior: 'smooth' })
+});
+
+// Page Navigation using Event bubbling and we want to use ecent capturing then we have to provide true on line 58 after }
+// This below piece of code is using event bubbling which is less efficient because everytime a nav__link is selected it alos triggers the nav__links and nav menas it's parent containers which can caus eperformance issues.
+// document.querySelectorAll('.nav__link').forEach((item) => {
+//   item.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   })
+// });
+
+// Page Navigation using Event delegation: which means we only provide handler only to the container which contains all the nav__link. 
+// Steps: 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', (event) => {
+  event.preventDefault()
+  if (event.target.classList.contains('nav__link')) {
+    // console.log(event.target.getAttribute('href'));
+    const id = event.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// const h1 = document.querySelector('h1');
+// const alertFunction = (event) => alert('I AM EVENT');
+// h1.addEventListener('mouseenter', alertFunction)
+// const randomInt = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// document.querySelector('.nav').addEventListener('click', function () {
+//   this.style.backgroundColor = randomColor()
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function () {
+//   this.style.backgroundColor = randomColor()
+// });
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // e.stopPropagation();
+// });
+// console.log(randomColor());
